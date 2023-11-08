99324d374d63594cda72b12dcd5d00ce99d47f05
TTNguyen25052003 <trantrongngyen2552003@gmail.com>
2023-11-08 15:07:26 +0700

    add Style

--- app/src/main/java/app/kotlin/littlelemon/ui/theme/Color.kt
+++ app/src/main/java/app/kotlin/littlelemon/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package app.kotlin.littlelemon.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object PrimaryColor {
+    val plateGreen = Color(0xFF485E56)
+    val sunflowerYellow = Color(0xFFF4CE13)
+}
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object SecondaryColor {
+    val coralPink = Color(0xFFED9971)
+    val palePink = Color(0xFFFADABA)
+}
+
+object HighlightColor{
+    val platinumGray =Color(0xFFECEEED)
+    val charcoalGray = Color(0xFF333333)
+}
--- app/src/main/java/app/kotlin/littlelemon/ui/theme/Type.kt
+++ app/src/main/java/app/kotlin/littlelemon/ui/theme/Type.kt
@@ -1,34 +1,73 @@
 package app.kotlin.littlelemon.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import app.kotlin.littlelemon.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+private val markaziTextFamily = FontFamily(
+    Font(R.font.markazitext_regular, FontWeight.Normal),
+    Font(R.font.markazitext_regular,FontWeight.Medium),
+    Font(R.font.markazitext_regular, FontWeight.SemiBold),
+    Font(R.font.markazitext_regular,FontWeight.Bold)
+)
+
+private val karlaFamily = FontFamily(
+    Font(R.font.karla_regular, FontWeight.ExtraLight),
+    Font(R.font.karla_regular, FontWeight.Light),
+    Font(R.font.karla_regular, FontWeight.Normal),
+    Font(R.font.karla_regular, FontWeight.Medium),
+    Font(R.font.karla_regular, FontWeight.Bold),
+    Font(R.font.karla_regular, FontWeight.ExtraBold)
+)
+
+val displayText = TextStyle(
+    fontFamily = markaziTextFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 64.sp
+)
+
+val subTitle = TextStyle(
+    fontFamily = markaziTextFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 40.sp
+)
+
+val leadText = TextStyle(
+    fontFamily = karlaFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp
+)
+
+val sectionTitle = TextStyle(
+    fontFamily = karlaFamily,
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 20.sp
+)
+
+val sectionCategory = TextStyle(
+    fontFamily = karlaFamily,
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 16.sp
+)
+
+val cardTitle = TextStyle(
+    fontFamily = karlaFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 18.sp
+)
+
+val paragraphText = TextStyle(
+    fontFamily = karlaFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    lineHeight = 28.sp
+)
+
+val highlightText = TextStyle(
+    fontFamily = karlaFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp
 )
